e39717f847186eb2b5251de874205cb4e838bb37
David Pavlov <davepavlov7@gmail.com>
2024-06-19 11:02:53 -0400

    requerments

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Bring in deps
 import os 
-from apikey import apikey 
+
 
 import streamlit as st 
 from langchain_openai.llms import OpenAI
@@ -8,7 +8,7 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain 
 from langchain.memory import ConversationBufferMemory
 from langchain_community.utilities import WikipediaAPIWrapper
-
+from apikey import apikey 
 os.environ['OPENAI_API_KEY'] = apikey
 
 # App framework
--- query_data_streamlit.py
+++ query_data_streamlit.py
@@ -31,19 +31,19 @@ Answer the question based on the above context: {question}
 """
 
 query_text = question
-st.write("working")
+# st.write("working")
 
 # Prepare the DB.
 embedding_function = OpenAIEmbeddings()
 db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
-st.write("done embeddings")
+# st.write("done embeddings")
 
 # Search the DB.
 results = db.similarity_search_with_relevance_scores(query_text, k=3)
 if len(results) == 0 or results[0][1] < 0.7:
     print(f"Unable to find matching results.")
 
-st.write("found match")
+# st.write("found match")
 context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
 prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
 prompt = prompt_template.format(context=context_text, question=query_text)
